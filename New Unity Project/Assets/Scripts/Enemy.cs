911801c53c2d7849e8b302e3b367262bb349acec
Jordan-M-Anderson <47698298+Jordan-M-Anderson@users.noreply.github.com>
2021-02-22 11:30:04 -0600

    Created an enemy script and added hit boxes to them

---
+++ New Unity Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]public int eHealth;
+    [SerializeField]public int enemyType;
+
+    public bool stuck = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (enemyType) {
+            case 0:
+                eHealth = 1;
+                break;
+            case 1:
+                eHealth = 2;
+                break;
+            case 2:
+                eHealth = 1;
+                //fly
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
