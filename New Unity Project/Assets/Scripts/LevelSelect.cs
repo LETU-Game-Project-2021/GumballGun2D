bc052ea2cf610b9c07c6dbc217f4c3e6d8daaff5
SeigiMitsuko <60911422+SeigiMitsuko@users.noreply.github.com>
2021-04-27 21:04:17 -0500

    Progress on Hub room
    Still need to figure out how to have it activate on user input.

---
+++ New Unity Project/Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+	private float oldTime = 1;
+	public GameObject select1UI;
+	// public GameObject select2UI;
+	// public GameObject select3UI;
+	// public GameObject select4UI;
+	// public GameObject select5UI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	public void select1()
+	{
+		oldTime = Time.timeScale;
+		select1UI.SetActive(true);
+		Time.timeScale = 0f;
+	}
+	public void exit1()
+	{
+		select1UI.SetActive(false);
+        Time.timeScale = oldTime;
+	}
+	
+	public void goToLevel1()
+	{
+		select1UI.SetActive(false);
+		Time.timeScale = 1f;
+		SceneManager.LoadScene("Level 1");
+	}
+	
+	
+}
